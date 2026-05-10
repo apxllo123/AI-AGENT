8e8c993f6d2eaf377550703bd73acb6c3bc5dc0a
apxllo123 <danwantstocode@outlook.com>
2026-05-09 23:13:03 -0500

    Import HugeTransformer in server.py

--- server/server.py
+++ server/server.py
@@ -7,6 +7,8 @@ import pickle
 import numpy as np
 from flask import Flask, jsonify, request, send_from_directory
 
+from model.huge_transformer import HugeTransformer
+
 REPO_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 if REPO_ROOT not in sys.path:
     sys.path.insert(0, REPO_ROOT)
