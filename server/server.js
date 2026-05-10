16c2d3c2b5e8019be1f46756b53bf223bef213f3
apxllo123 <danwantstocode@outlook.com>
2026-05-10 16:26:18 -0500

    Change server port from 3000 to 8080

--- server/server.js
+++ server/server.js
@@ -35,7 +35,7 @@ app.post("/chat", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
