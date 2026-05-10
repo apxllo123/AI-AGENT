3527fa1213238370f028b7f9cf079b53b9911ce4
apxllo123 <danwantstocode@outlook.com>
2026-05-10 16:59:29 -0500

    Refactor model training parameters in train.py

--- train.py
+++ train.py
@@ -2,7 +2,7 @@ import os
 import pickle
 
 from tokenizer.transformer import SimpleBPE
-from model.huge_transformer import train_huge_model as train_tiny_model_numpy
+from model.huge_transformer import train_tiny_model_numpy
 
 os.makedirs("artifacts", exist_ok=True)
 
@@ -16,7 +16,13 @@ bpe = SimpleBPE()
 bpe.train(corpus, num_merges=12)
 vocab_size = len(bpe.vocab)
 
-model = train_tiny_model_numpy(bpe, vocab_size, text_corpus=corpus)
+model = train_tiny_model_numpy(
+    bpe=bpe,
+    vocab_size=vocab_size,
+    text_corpus=corpus,
+    steps=2000,
+    lr=1e-2,
+)
 
 with open("artifacts/bpe.pkl", "wb") as f:
     pickle.dump(bpe, f)
