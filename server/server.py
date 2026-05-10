31c0842c342858e6b38b602c79be2d111bd63787
apxllo123 <danwantstocode@outlook.com>
2026-05-09 21:31:11 -0500

    Change server port from 5000 to 8080

--- server/server.py
+++ server/server.py
@@ -81,5 +81,5 @@ def chat():
 
 if __name__ == "__main__":
     load_model()
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port, debug=False)
